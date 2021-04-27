5b160b0d7af0122d0d7eae5c44d62fb0ce199ab3
Diego223 <47160448+Diego223@users.noreply.github.com>
2021-04-27 15:34:52 -0600

    Add files via upload

---
+++ SRC/index.js
@@ -0,0 +1,27 @@
+import "./style.css";
+
+class Calculator {
+    constructor(sigoperando, actualoperando) {
+      this.sigoperando = sigoperando
+      this.actualoperando = actualoperando
+      this.clear()
+    }
+  
+    clear() {
+      this.sigoperando = ''
+      this.actualoperando= ''
+      this.operador = undefined
+    }
+  
+    delete() {
+     
+    }
+  
+    agregarnumero(numero) {
+     
+    }
+  
+    operacion(operador) {
+      
+    }
+}
